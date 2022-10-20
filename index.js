b9276e8ef46a7db9ba24fe372e88f72df2ec1a9d
Callum Taylor <callumtaylor@DESKTOP-FK764BF.localdomain>
2022-10-20 22:40:50 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+import { ethers } from "./ethers-5.1.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        await window.ethereum.request({method: "eth_requestAccounts"})
+        connectButton.innerHTML = "Connected"
+    } else {
+        connectButton.innerHTML = "Please Install Metamask"
+    }
+}
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
+
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({ value: ethers.utils.parseEther(ethAmount) })
+            await listForTransactionMind(transactionResponse, provider)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+}
+
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw();
+            await listForTransactionMind(transactionResponse, provider)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+    }
+}
+
+function listForTransactionMind(transactionResponse, provider) {
+     return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReciept) => {
+            console.log(`Completed with ${transactionReciept.confirmations} confirmations`)
+        })
+        resolve()
+        reject((err) => {
+            console.log(err)
+        })
+    })
+}
